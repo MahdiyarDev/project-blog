dc10ff0f7f218e132c2f121463ac19375b1aa966
git.mahdiyar@gmail.com <git.mahdiyar@gmail.com>
2025-12-30 10:42:12 -0500

    add file java script

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -1,13 +1,10 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>my blog</title>
-    <link rel="stylesheet" href="/public/style.css">
+    <link rel="stylesheet" href="/style.css" />
 </head>
 <body>
     <h1>my blog</h1>
@@ -16,7 +13,7 @@
         <a href="/posts">Posts</a>
     </nav>
     <main>
-        <?= $content ?>
+        <?=$content ?>
     </main>
     <footer>
         <p> &copy; <?= date('Y') ?> my blog </p>
