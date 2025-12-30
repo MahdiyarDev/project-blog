eabd1ff8d67c5cf2cf08be9c5430b5f15f795e13
git.mahdiyar@gmail.com <git.mahdiyar@gmail.com>
2025-12-30 09:28:01 -0500

    added html template main

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>my blog</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/posts">Posts</a>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+        <p> &copy; <?= date('Y') ?> my blog </p>
+    </footer>
+</body>
+</html>
