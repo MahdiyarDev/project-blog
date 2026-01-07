3d2d1d294231ab3a2ec6b566e3692c4b7b1ca35c
git.mahdiyar@gmail.com <git.mahdiyar@gmail.com>
2026-01-07 13:59:40 -0500

    add folder Models

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -5,7 +5,6 @@ use Core\View;
 
 class HomeController{
     public function index(){
-        throw new Exception("this has heppened in the web");
         return View::render(
         template: 'home/index',
         data: ['message' => 'hello mahdiyar'],
