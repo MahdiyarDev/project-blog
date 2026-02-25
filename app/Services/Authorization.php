d25546f8057f402ee8a77775702d507b7dd41d35
git.mahdiyar@gmail.com <git.mahdiyar@gmail.com>
2026-02-25 12:40:59 -0500

    add the Authorization and 403 error page

--- app/Services/Authorization.php
+++ app/Services/Authorization.php
@@ -1,3 +1,35 @@
 <?php
 
-class Authorization{}
+namespace App\Services;
+
+use App\Services\Auth; 
+use App\Models\Post;
+use Core\Router;
+
+class Authorization{
+
+    public static function verify(string $action, mixed $resource = null): void{
+        if(!static::check($action,$resource)){
+            Router::forbidden();
+        }
+    }
+
+    public static function check(string $action, mixed $resource = null): bool{
+        $user = Auth::user();
+
+        if(!$user){
+            return false;
+        }
+
+        if('superAdmin' === $user->role){
+            return true;
+        }
+
+        return match($action){
+            'dashboard' => in_array($user->role , ['admin' , 'superAdmin']),
+            'edit_post', 'delete_post' => $resource instanceof Post && (($user->id === $resource->user_id) || in_array($user->role , ['admin' , 'superAdmin'])),
+            'comment' , 'create_post' => true,
+            default => false
+        };
+    }
+}
---
+++ app/Views/errors/403.php
@@ -0,0 +1,2 @@
+<h1>403 - forbidden</h1>
+<p>You dont have permissions to acces this resource | <a href="/login">Login page</a> </p>
